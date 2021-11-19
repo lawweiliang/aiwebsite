68217f4c11159b0253a6dfeb869f4881790087fd
liang <yeslaw90@hotmail.com>
2021-11-19 13:58:32 +0800

    setup components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { CTA, Brand, Navbar } from './components';
+
+const App = () => {
+  return (
+    <div>
+      <div>App Now</div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './navbar.css';
+
+function Nabbar() {
+  return (
+    <div>
+      <h1>Nabbar</h1>
+    </div>
+  );
+}
+
+export default Nabbar;
