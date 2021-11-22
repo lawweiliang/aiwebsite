1b1b66b174fae31e631f48ea35d3ffef2652edfe
liang <yeslaw90@hotmail.com>
2021-11-23 06:19:17 +0800

    finished header components

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -6,21 +6,25 @@ import './header.css';
 function Header() {
   return (
     <div className="gpt3__header section__padding" id="home">
-      <div className="gpt3_header-content">
-        <h1>Let’s Build Something amazing with GPT-3 OpenAI</h1>
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">Let’s Build Something amazing with GPT-3 OpenAI</h1>
         <p>
           Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration
           boisterous the attachment. Party we years to order allow asked of.
         </p>
 
         <div className="gpt3__header-content__input">
-          <input type="text" placeholder="Your Email Address" />
-          <button>Get Started</button>
+          <input type="email" placeholder="Your Email Address" />
+          <button type="button">Get Started</button>
         </div>
 
-        <div className="gpt3__header-content__people"></div>
+        <div className="gpt3__header-content__people">
+          <img src={people} alt="people.png" />
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
       </div>
-      <div className="gpt3__header-content__image">
+
+      <div className="gpt3__header-image">
         <img src={ai} alt="ai.png" />
       </div>
     </div>
