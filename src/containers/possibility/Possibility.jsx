87fd0db12a3a7fb923aa3e7a4374609dc8ed8b95
liang <yeslaw90@hotmail.com>
2021-11-22 17:18:00 +0800

    add container components

---
+++ src/containers/possibility/Possibility.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './possibility.css';
+
+function Possibility() {
+  return (
+    <div>
+      <h1>Possibility</h1>
+    </div>
+  );
+}
+
+export default Possibility;
---
+++ src/containers/whatGPT3/WHATGPT3.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './WHATGPT3.css';
+
+function WHATGPT3() {
+  return (
+    <div>
+      <h1>WHATGPT3</h1>
+    </div>
+  );
+}
+
+export default WHATGPT3;
