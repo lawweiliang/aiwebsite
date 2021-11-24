58b75dbae38b72908ceb3f1b0f14228d27eb1dfb
liang <yeslaw90@hotmail.com>
2021-11-25 00:24:19 +0800

    footer components

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
+import gpt3Logo from '../../assets/logo.svg';
 import './footer.css';
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className="gpt3__footer section__padding">
+      <div className="gpt3__footer-heading">
+        <h1 className="gradient__text">Do you want to step in to the future before others</h1>
+      </div>
+      <div className="gpt3__footer-btn">
+        <p>Request Early Access</p>
+      </div>
+      <div className="gpt3__footer-links">
+        <div className="gpt3__footer-links__logo">
+          <img src={gpt3Logo} alt="" />
+          <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+        </div>
+        <div className="gpt3__footer-links__linklist">
+          <h4>Links</h4>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links__linklist">
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links__linklist">
+          <h4>Get in touch</h4>
+          <p>Crechterwoord K12 182 DK Alknjkcb</p>
+          <p>085-132567</p>
+          <p>info@payme.net</p>
+        </div>
+      </div>
+      <div className="gpt3__footer__copyright">
+        <p>© 2021 GPT-3. All rights reserved.</p>
+      </div>
     </div>
   );
 };
